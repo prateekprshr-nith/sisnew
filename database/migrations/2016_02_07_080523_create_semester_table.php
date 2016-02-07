6bd74ee0530bd678461891a4991cb7d226a72c8f
Prateek Prasher <prateekprshr@gmail.com>
2016-02-07 13:42:36 +0530

    Added semesters table

---
+++ database/migrations/2016_02_07_080523_create_semester_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSemesterTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('semesters', function (Blueprint $table) {
+            $table->decimal('semNo', '2');
+            $table->timestamps();
+
+            // Key constraints
+            $table->primary('semNo');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('semesters');
+    }
+}
