66f875177ec9931d6ea83235a79904567822ed0d
Prateek Prasher <prateekprshr@gmail.com>
2016-02-07 16:42:53 +0530

    Added teachers table

---
+++ database/migrations/2016_02_07_105547_create_teachers_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeachersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teachers', function (Blueprint $table)
+        {
+            $table->string('fId', '20');
+            $table->string('fName', '100');
+            $table->string('dCode', '10');
+            $table->string('email', '50');
+            $table->decimal('phoneNo', '10');
+            $table->string('office', '200');
+            $table->string('password', '50');
+            $table->timestamps();
+
+            // Key constraints
+            $table->primary('fId');
+            $table->unique(['phoneNo', 'email']);
+            $table->foreign('dCode')
+                  ->references('dCode')
+                  ->on('departments')
+                  ->onDelete('cascade')
+                  ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('teachers');
+    }
+}
