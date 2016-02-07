abed4a4be27af0ac81c66d6df6c08f91ccbdfbaa
Prateek Prasher <prateekprshr@gmail.com>
2016-02-07 14:24:25 +0530

    Added sections table

---
+++ database/migrations/2016_02_07_081855_create_section_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSectionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sections', function (Blueprint $table)
+        {
+            $table->string('sectionId', '10');
+            $table->string('dCode', '10');
+            $table->timestamps();
+
+            // Key constraints
+            $table->primary('sectionId');
+            $table->foreign('dCode')
+                  ->references('dCode')
+                  ->on('departments')
+                  ->onUpdate('cascade')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('sections');
+    }
+}
