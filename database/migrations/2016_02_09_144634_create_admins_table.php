0bae3f78f3d138c00c18e9bdc6abde8a5723afdc
Prateek Prasher <prateekprshr@gmail.com>
2016-02-09 20:21:26 +0530

    Added admins table

---
+++ database/migrations/2016_02_09_144634_create_admins_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdminsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table)
+        {
+            $table->string('id', '20');
+            $table->foreign('password', '50');
+            $table->timestamps();
+
+            // Key constraints
+            $table->primary('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('admins');
+    }
+}
