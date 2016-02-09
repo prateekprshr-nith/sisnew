25b33ba36ca34095708c18da4f14fbe54bcec522
Prateek Prasher <prateekprshr@gmail.com>
2016-02-09 18:27:29 +0530

    Added academicRecords table

---
+++ database/migrations/2016_02_09_122750_create_academic_records_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAcademicRecordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('academicRecords', function (Blueprint $table)
+        {
+            $table->string('rollNo', '20');
+            $table->string('courseCode', '10');
+            $table->smallInteger('attendance');
+            $table->smallInteger('periodicalMarks');
+            $table->smallInteger('asnMarks');
+            $table->smallInteger('finalMarks');
+            $table->smallInteger('totalMarks');
+            $table->timestamps();
+
+            // Key constraints
+            $table->primary('rollNo');
+            $table->foreign('rollNo')
+                  ->references('rollNo')
+                  ->on('students')
+                  ->onDelete('cascade')
+                  ->onUpdate('cascade');
+            $table->foreign('courseCode')
+                  ->references('courseCode')
+                  ->on('courses')
+                  ->onDelete('cascade')
+                  ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('academicRecords');
+    }
+}
