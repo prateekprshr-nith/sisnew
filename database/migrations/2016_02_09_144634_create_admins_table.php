baef093351c78428d7f5418e7909be0430ba62df
Prateek Prasher <prateekprshr@gmail.com>
2016-02-09 23:19:54 +0530

    Fixed a typo in create admin table migration

--- database/migrations/2016_02_09_144634_create_admins_table.php
+++ database/migrations/2016_02_09_144634_create_admins_table.php
@@ -15,7 +15,7 @@ class CreateAdminsTable extends Migration
         Schema::create('admins', function (Blueprint $table)
         {
             $table->string('id', '20');
-            $table->foreign('password', '50');
+            $table->string('password', '50');
             $table->timestamps();
 
             // Key constraints
