e75d0c10caf42bcedf67032946bb5c1d020822d1
Prateek Prasher <prateekprshr@gmail.com>
2016-02-09 20:11:17 +0530

    Added days table

---
+++ database/migrations/2016_02_09_131159_create_days_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('days', function (Blueprint $table)
+        {
+            $table->string('day');
+            $table->timestamps();
+
+            // Key constraints
+            $table->primary('day');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('days');
+    }
+}
