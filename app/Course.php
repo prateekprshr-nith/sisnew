236b2458cdde37a4c94da171bf0b3714ce4b055f
Prateek Prasher <prateekprshr@gmail.com>
2016-02-11 23:48:45 +0530

    Added department relation

--- app/Course.php
+++ app/Course.php
@@ -4,8 +4,23 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Course, this model for our courses table
+ *
+ * @package App
+ */
 class Course extends Model
 {
     protected $primaryKey = 'courseCode';
     public $incrementing = false;
+
+    /**
+     * Get the department of the course
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function department()
+    {
+        return $this->belongsTo('App\Department', 'dCode', 'dCode');
+    }
 }
