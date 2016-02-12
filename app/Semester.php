3f0595129b96b7da07f6b3eaf54b261980bacf46
Prateek Prasher <prateekprshr@gmail.com>
2016-02-12 17:38:05 +0530

    Added semester timetable relation

--- app/Semester.php
+++ app/Semester.php
@@ -4,8 +4,23 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Semester, this is our mode for semester table
+ *
+ * @package App
+ */
 class Semester extends Model
 {
     protected $primaryKey = 'semNo';
     public $incrementing = false;
+
+    /**
+     * Get the time table
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function timeTable()
+    {
+        return $this->hasMany('App\TimeTable', 'semNo', 'semNo');
+    }
 }
--- app/TimeTable.php
+++ app/TimeTable.php
@@ -4,8 +4,23 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class TimeTable, this is our model for timeTable table.
+ *
+ * @package App
+ */
 class TimeTable extends Model
 {
     protected $primaryKey = ['sectionId', 'semNo', 'day'];
     public $incrementing = false;
+
+    /**
+     * Get the semester
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function semester()
+    {
+        return $this->belongsTo('App\Semester', 'semNo', 'semNo');
+    }
 }
