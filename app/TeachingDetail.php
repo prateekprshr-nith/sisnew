209b2a271a6468bf72a2bc7a697ab54afb8abc87
Prateek Prasher <prateekprshr@gmail.com>
2016-02-12 17:16:32 +0530

    Added teacher relation

--- app/TeachingDetail.php
+++ app/TeachingDetail.php
@@ -4,9 +4,24 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class TeachingDetail, this is our model for teachingDetails table
+ *
+ * @package App
+ */
 class TeachingDetail extends Model
 {
     protected $table = 'teachingDetails';
     protected $primaryKey = 'courseCode';
     public $incrementing = false;
+
+    /**
+     * Get the teacher
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function teacher()
+    {
+        return $this->belongsTo('App\Teacher', 'fId', 'fId');
+    }
 }
