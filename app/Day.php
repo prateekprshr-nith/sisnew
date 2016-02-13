5c854cfe469ddc73ae8a349d7a49f8d761c26a95
Prateek Prasher <prateekprshr@gmail.com>
2016-02-13 12:27:25 +0530

    Added day timeTable relation

--- app/Day.php
+++ app/Day.php
@@ -4,8 +4,23 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Day, this is our model for day table
+ *
+ * @package App
+ */
 class Day extends Model
 {
     protected $primaryKey = 'day';
     public $incrementing = false;
+
+    /**
+     * Get the time table
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function timeTables()
+    {
+        return $this->hasMany('App\TimeTable', 'day', 'day');
+    }
 }
