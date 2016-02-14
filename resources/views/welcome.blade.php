f0a77b5ac925b26b8685377de8f310c3e2a52388
Prateek Prasher <prateekprshr@gmail.com>
2016-02-15 00:40:29 +0530

    Added a welcome view

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="container-fluid">
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <h1 class="text-center text-muted">Wecome to SIS</h1>
+            </div>
+            <div class="panel-body">
+                <h1 class="text-center text-muted">Wecome to SIS</h1>
+            </div>
+            <div class="panel-body">
+                <h1 class="text-center text-muted">Wecome to SIS</h1>
+            </div>
+            <div class="panel-body">
+                <h1 class="text-center text-muted">Wecome to SIS</h1>
+            </div>
+            <div class="panel-body">
+                <h1 class="text-center text-muted">Wecome to SIS</h1>
+            </div>
+        </div>
+    </div>
+@endsection
