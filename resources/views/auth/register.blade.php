747d9456d1708b785ff8c3a57bc455d725b58d43
Prateek Prasher <prateekprshr@gmail.com>
2016-02-15 22:18:33 +0530

    Added registration view

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,197 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="container col-sm-offset-2 col-sm-8">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Registration: Please enter your details correctly
+            </div>
+            <div class="panel-body">
+                <form method="POST" action="/auth/register" accept-charset="UTF-8" id="registerForm"><input required name="_token" type="hidden">
+                    {{ csrf_field() }}
+
+                    <!-- Display Validation Errors -->
+                    @include('common.errors')
+
+                     <!-- First row Name and Email-->
+                    <div class="row">
+                        <div class="form-group row col-md-6 center-block">
+                            <div class="col-md-4 text-right ">
+                                <label for="name">Name:</label>
+                            </div>
+                            <div class="col-md-8">
+                                <input required class="form-control" name="name" type="text" id="name">
+                            </div>
+                        </div>
+                        <div class="form-group row col-md-6 center-block">
+                            <div class="col-md-4 text-right">
+                                <label for="email" class="text-right">Email:</label>
+                            </div>
+                            <div class="col-md-8">
+                                <input required class="form-control" name="email" type="email" id="email">
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Second row Roll no and semester -->
+                    <div class="row">
+                        <div class="form-group row col-md-6 center-block">
+                            <div class="col-md-4 text-right">
+                                <label for="rollNo">Roll No:</label>
+                            </div>
+                            <div class="col-md-8">
+                                <input required class="form-control" name="rollNo" type="text" id="rollNo">
+                            </div>
+                        </div>
+                        <div class="form-group row col-md-6 center-block">
+                            <div class="col-md-4 text-right">
+                                <label for="semester" class="text-right">Semester:</label>
+                            </div>
+                            <div class="col-md-8">
+                                <input required class="form-control" name="semester" type="text" id="semester">
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Third row Department and Section-->
+                    <div class="row">
+                        <div class="form-group row col-md-6 center-block">
+                            <div class="col-md-4 text-right">
+                                <label for="department">Department:</label>
+                            </div>
+                            <div class="col-md-8">
+                                <select required id="department" name="department" class="form-control">
+                                    <option value="">Select a Department...</option>
+                                    <option value="CSED">Computer Science and Engineering Dept.</option>
+                                    <option value="ECED">Electronics and Communication Engineering Dept.</option>
+                                    <option value="EED">Electrical and Electronics Engineering Dept.</option>
+                                    <option value="MED">Mechanical Engineering Dept.</option>
+                                    <option value="CED">Civil Engineering Dept.</option>
+                                    <option value="CHED">Chemical Engineering Dept.</option>
+                                    <option value="ARD">Architecture Dept.</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group row col-md-6 center-block">
+                            <div class="col-md-4 text-right">
+                                <label for="section" class="text-right">Section:</label>
+                            </div>
+                            <div class="col-md-8">
+                                <select required class="form-control" name="section" id="section">
+                                    <option value="">Select a Section...</option>
+                                    <option value="A1">A1</option>
+                                    <option value="A2">A2</option>
+                                    <option value="A3">A3</option>
+                                    <option value="B1">B1</option>
+                                    <option value="B2">B2</option>
+                                    <option value="B3">B3</option>
+                                    <option value="C1">C1</option>
+                                    <option value="C2">C2</option>
+                                    <option value="C3">C3</option>
+                                    <option value="D1">D1</option>
+                                    <option value="D2">D2</option>
+                                    <option value="D3">D3</option>
+                                    <option value="E1">E1</option>
+                                    <option value="E2">E2</option>
+                                    <option value="F1">F1</option>
+                                    <option value="F2">F2</option>
+                                    <option value="G1">G1</option>
+                                    <option value="G2">G2</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Fourth row DOB no and phone number-->
+                    <div class="row">
+                        <div class="form-group row col-md-6 center-block">
+                            <div class="col-md-4 text-right">
+                                <label for="dob">Birth Date:</label>
+                            </div>
+                            <div class="col-md-8">
+                                <input required class="form-control" name="dob" type="date" id="dob">
+                            </div>
+                        </div>
+                        <div class="form-group row col-md-6 center-block">
+                            <div class="col-md-4 text-right">
+                                <label for="phoneNo" class="text-right">Phone No:</label>
+                            </div>
+                            <div class="col-md-8">
+                                <input required class="form-control" name="phoneNo" type="tel" id="phoneNo">
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Fifth row address-->
+                    <div class="row">
+                        <div class="form-group row col-md-6 center-block">
+                            <div class="col-md-4 text-right">
+                                <label for="address">Address:</label>
+                            </div>
+                            <div class="col-md-8">
+                                <textarea required class="form-control" rows="2" name="address" type="text" id="address">
+
+                                </textarea>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Sixth row passwords-->
+                    <div class="row">
+                        <div class="form-group row col-md-6 center-block">
+                            <div class="col-md-4 text-right ">
+                                <label for="password">Password:</label>
+                            </div>
+                            <div class="col-md-8">
+                                <input required class="form-control" name="password" type="password" id="password" onkeyup="checkPassword()">
+                            </div>
+                        </div>
+                        <div class="form-group row col-md-6 center-block">
+                            <div class="col-md-4 text-right ">
+                                <label for="confirmPassword">Confirm:</label>
+                            </div>
+                            <div class="col-md-8">
+                                <input required class="form-control" name="confirmPassword" type="password" id="confirmPassword" onkeyup="checkPassword()">
+                            </div>
+                        </div>
+                    </div>
+
+                    <!--Seventh row error msg-->
+                    <div class="row">
+                        <div class="form-group row col-md-6 center-block">
+                            <div class="col-md-4 text-right ">
+                            </div>
+                            <div class="col-md-8">
+                                <p id="passwordErrorMsg"></p>
+                            </div>
+                        </div>
+
+                    </div>
+                    <!-- Seventh row Register-->
+                    <div class="row">
+                        <div class="form-group row col-md-6 center-block">
+                            <div class="col-md-4 text-right ">
+                            </div>
+                            <div class="col-md-4 center-block">
+                                <button class="btn btn-default" type="submit" disabled id="registerButton">
+                                    <span class="glyphicon glyphicon-user"></span> Register
+                                </button>
+                            </div>
+                        </div>
+                        <div class="form-group row col-md-6 center-block">
+                            <div class="col-md-4 text-right ">
+                            </div>
+                            <div class="col-md-4 center-block">
+                                <button class="btn btn-default" onclick="reset()">
+                                    <span class="glyphicon glyphicon-refresh"></span> Reset
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+
+                </form>
+            </div>
+
+        </div>
+    </div>
+@endsection
