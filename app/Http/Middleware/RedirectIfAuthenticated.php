c1749e6b4d49138d80b4fdfa06a618907c62c9d8
Prateek Prasher <prateekprshr@gmail.com>
2016-04-02 14:15:10 +0530

    Added code for redirecting all users

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,8 +17,21 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if (Auth::guard($guard)->check()) {
-            return redirect('/');
+        if (Auth::guard($guard)->check())
+        {
+            // Check for the guard and redirect accordingly
+            if($guard == 'student')
+            {
+                return redirect('/students/home');
+            }
+            else if($guard == 'teacher')
+            {
+                return redirect('/teachers/home');
+            }
+            else if($guard == 'admin')
+            {
+                return redirect('/admins/home');
+            }
         }
 
         return $next($request);
