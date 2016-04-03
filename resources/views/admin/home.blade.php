5908917f046a7609d55493ae98bff3867b7453c5
jaykay12 <jalazkumar1208@gmail.com>
2016-04-03 14:59:10 +0530

    Changed layout

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,11 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-fluid">
+<div class="container">
+    <div class="jumbotron">
+        <h1>Welcome Admin</h1>
+        <p> Manage all the sections of the System Information portal from here</p>
+    </div>
 
     <!-- First panel: Manage users -->
-    <div class="col-md-12">
-        <div class="panel panel-default">
+    <div class="col-md-6 col-md-offset-3">
+        <div class="panel panel-info">
             <div class="panel-heading">
                 <span class="glyphicon glyphicon-dashboard"></span>
                 <strong> Manage Users</strong>
@@ -15,7 +19,7 @@
                     <a href="/admins/manage/teachers" class="btn btn-default">
                         <span class="glyphicon glyphicon-user"></span> Manage Teachers
                     </a>
-                    <a href="/admins/manage/hostelStaffs" class="btn btn-default">
+                <!--    <a href="/admins/manage/hostelStaffs" class="btn btn-default">
                         <span class="glyphicon glyphicon-user"></span> Manage Hostel Staff
                     </a>
                     <a href="/admins/manage/adminStaffs" class="btn btn-default">
@@ -26,7 +30,8 @@
                     </a>
                     <a href="/admins/manage/chiefWardenStaffs" class="btn btn-default">
                         <span class="glyphicon glyphicon-user"></span> Manage ChiefWarden Staff
-                    </a>
+                    </a> -->
+
                     <a href="/admins/manage/students" class="btn btn-default">
                         <span class="glyphicon glyphicon-user"></span> Manage Students
                     </a>
@@ -36,8 +41,8 @@
     </div>
 
     <!-- Second panle: Manage Departments, Sections and Hostels-->
-    <div class="col-md-12">
-        <div class="panel panel-default">
+    <div class="col-md-8 col-md-offset-2">
+        <div class="panel panel-info">
             <div class="panel-heading">
                 <span class="glyphicon glyphicon-dashboard"></span>
                 <strong> Manage Departments, Sections and Hostels</strong>
