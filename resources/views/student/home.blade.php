c46c1659dd8f6797a858bdc7af9b2c805bb82345
jaykay12 <jalazkumar1208@gmail.com>
2016-04-03 18:00:27 +0530

    Teacher Student Home Page

--- resources/views/student/home.blade.php
+++ resources/views/student/home.blade.php
@@ -5,13 +5,40 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading"><h2>WELCOME STUDENT</h2></div>
+            </div>
+        </div>
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-info">
+                <div class="panel-heading">Personal Information</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    Name:<br>
+                    Roll No:<br>
+                    Department:<br>
+                    Email Id:<br>
+                    Hostel:<br>
+                    Contact:<br>
+                    Current Address: <br>
+                    Permanent Address: <br>
                 </div>
             </div>
         </div>
+
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-info">
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+                    <button class="btn-primary">Attendence</button>
+                    <button class="btn-primary">Assignments</button>
+                    <button class="btn-primary">Notices</button>
+                    <button class="btn-primary">Messages</button>
+                    <button class="btn-primary">Results</button>
+                </div>
+            </div>
+        </div>
+
     </div>
 </div>
 @endsection
--- resources/views/teacher/home.blade.php
+++ resources/views/teacher/home.blade.php
@@ -5,10 +5,34 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
+                <div class="panel-heading"><h2>WELCOME USER</h2></div>
+            </div>
+        </div>
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-info">
+                <div class="panel-heading">Personal Information</div>
+
+                <div class="panel-body">
+                    Name:<br>
+                    FacultyID:<br>
+                    Department:<br>
+                    Email Id:<br>
+                    Office:<br>
+                    Contact:<br>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-info">
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    Teacher, you are logged in!
+                    <button class="btn-primary">Attendence</button>
+                    <button class="btn-primary">Assignments</button>
+                    <button class="btn-primary">Queries</button>
+                    <button class="btn-primary">Results</button>
                 </div>
             </div>
         </div>
