c91a819390a8997f9df70bdd4d5d267a2fef779a
Prateek Prasher <prateekprshr@gmail.com>
2016-04-04 14:38:07 +0530

    Modified students and teachers table

--- database/migrations/2016_02_07_091814_create_students_table.php
+++ database/migrations/2016_02_07_091814_create_students_table.php
@@ -3,6 +3,10 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
+/**
+ * Class CreateStudentsTable, this migration
+ * creates the 'students' table in database
+ */
 class CreateStudentsTable extends Migration
 {
     /**
@@ -15,15 +19,22 @@ class CreateStudentsTable extends Migration
         Schema::create('students', function (Blueprint $table)
         {
             $table->string('rollNo', '20');
-            $table->string('sName', '100');
+            $table->string('registrationNo', '20');
+            $table->string('name', '100');
+            $table->string('fatherName', '100');
+            $table->string('motherName', '100');
             $table->string('dCode', '10');
             $table->string('email', '50');
             $table->string('phoneNo', '15');
-            $table->string('address', '200');
+            $table->string('currentAddress', '200');
+            $table->string('permanentAddress', '200');
             $table->smallInteger('semNo');
             $table->string('sectionId', '10');
             $table->string('password', '100');
             $table->date('dob');
+            $table->string('verificationCode', '100');
+            $table->boolean('verified');
+            $table->rememberToken();
             $table->timestamps();
 
             // Key constraints
@@ -31,20 +42,20 @@ class CreateStudentsTable extends Migration
             $table->unique('phoneNo');
             $table->unique('email');
             $table->foreign('dCode')
-                  ->references('dCode')
-                  ->on('departments')
-                  ->onDelete('cascade')
-                  ->onUpdate('cascade');
+                ->references('dCode')
+                ->on('departments')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
             $table->foreign('semNo')
-                  ->references('semNo')
-                  ->on('semesters')
-                  ->onDelete('cascade')
-                  ->onUpdate('cascade');
+                ->references('semNo')
+                ->on('semesters')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
             $table->foreign('sectionId')
-                  ->references('sectionId')
-                  ->on('sections')
-                  ->onDelete('cascade')
-                  ->onUpdate('cascade');
+                ->references('sectionId')
+                ->on('sections')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
         });
     }
 
