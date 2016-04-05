d82266d334fc313a8c5d96aeb2dac043843c91d5
Prateek Prasher <prateekprshr@gmail.com>
2016-04-06 00:49:07 +0530

    Added courseSelection view for teacher

---
+++ resources/views/teacher/courseSelection.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container-fluid">
+
+        <!-- Select a course-->
+        <div class="col-md-4 col-md-offset-4">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <span class="glyphicon glyphicon-plus"></span>
+                    <strong> Select a course to view student record</strong>
+                </div>
+
+                <div class="panel-body">
+                    <div class="input-group input-group-sm">
+                        <select required id="courseCode" name="courseCode" class="form-control">
+                            <option value="">Select a Course...</option>
+                            @foreach($teacherCourses as $teacherCourse)
+                                <option value="{{$teacherCourse->courseCode}}">{{$teacherCourse->course->courseName}}</option>
+                            @endforeach
+                        </select>
+
+                        <span class="input-group-btn">
+                            <a href="#" id="studentRecordLink" class="btn btn-primary"
+                               onclick='setLinkUrl("studentRecordLink", "/teachers/studentRecords/" + document.getElementById("courseCode").value)'>
+                                <span class="glyphicon glyphicon-plus"></span> View Record
+                            </a>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
