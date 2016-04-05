a179e835a960d55c56006df11c8d422a2a17a312
Prateek Prasher <prateekprshr@gmail.com>
2016-04-06 02:23:50 +0530

    Added studentRecord view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -67,7 +67,7 @@
                         @elseif($user == 'teacher')
                             <li><a href="/teachers/home">Home</a></li>
                             <li><a href="/teachers/courses">Teaching Information</a></li>
-                            <li><a href="#">Student Records</a></li>
+                            <li><a href="/teachers/studentRecords">Student Records</a></li>
                             <li><a href="#">Assignments</a></li>
                             <li><a href="#">Doubt corner</a></li>
                         @endif
