f9183052e1d7ac152b93981171ec06e2e74ccbaa
Prateek Prasher <prateekprshr@gmail.com>
2016-04-06 18:49:39 +0530

    Added student queries model and db table

---
+++ database/migrations/2016_04_06_103331_create_student_queries_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * Class CreateStudentQueriesTable, this class
+ * creates a studentQueries database table
+ */
+class CreateStudentQueriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('studentQueries', function (Blueprint $table) {
+            $table->string('rollNo', '20');
+            $table->string('courseCode', '10');
+            $table->string('description', '1000')->nullable();
+            $table->string('response', '1000')->nullable();
+            $table->timestamps();
+
+            // Key constraints
+            $table->primary(['rollNo', 'courseCode']);
+            $table->foreign(['rollNo', 'courseCode'])
+                ->references(['rollNo', 'courseCode'])
+                ->on('academicRecords')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('studentQueries');
+    }
+}
